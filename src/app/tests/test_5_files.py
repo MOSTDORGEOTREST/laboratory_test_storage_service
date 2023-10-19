a0978472b7b954855c4ca058b1e67fc494c89ad3
tnick1502 <tnick1502@mail.ru>
2023-10-19 18:25:49 +0300

    fix: file tests

--- src/app/tests/test_5_files.py
+++ src/app/tests/test_5_files.py
@@ -1,14 +1,38 @@
 from httpx import AsyncClient
 
-async def test_file_create(ac: AsyncClient, test_tests, test_wrong_test):
+async def test_file_create(ac: AsyncClient):
     response = await ac.post(
         "/tests/files/?test_id=0",
-        files={'test': open('./tests/file.png', 'rb')}
+        files={'file': open('./tests/file.png', 'rb')}
     )
     assert response.status_code == 404
 
     response = await ac.post(
         "/tests/files/?test_id=2",
-        files={'test': open('./tests/file.png', 'rb')}
+        files={'file': open('./tests/file.png', 'rb')}
     )
     assert response.status_code == 200
+
+    response = await ac.post(
+        "/tests/files/?test_id=2",
+        files={'file': open('./tests/file.png', 'rb')}
+    )
+    assert response.status_code == 200
+
+async def test_file_get(ac: AsyncClient):
+    response = await ac.get(
+        "tests/files/?test_id=2"
+    )
+    assert response.status_code == 200
+
+async def test_file_delete(ac: AsyncClient):
+    response = await ac.delete(
+        "tests/files/?test_id=2"
+    )
+    assert response.status_code == 204
+
+    response = await ac.get(
+        "tests/files/?test_id=2"
+    )
+    assert response.status_code == 404
+
