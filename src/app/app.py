90d522de81e3050a99dcaf90676811057847a620
Olesyahristenko <Olesyahristenkojob@yandex.ru>
2023-11-03 16:26:04 +0300

    feat: страница входа в лс

--- src/app/app.py
+++ src/app/app.py
@@ -1,12 +1,17 @@
-from fastapi import FastAPI
+import os
+
+from fastapi import FastAPI, Request, HTTPException, status
 from starlette.middleware.cors import CORSMiddleware
 from fastapi.responses import (
     HTMLResponse,
     JSONResponse
 )
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from redis import asyncio as aioredis
+from fastapi.security.utils import get_authorization_scheme_param
 
 from config import configs
 from database.database import (
@@ -46,9 +51,26 @@ app.add_middleware(
 
 app.include_router(router)
 
+script_dir = os.path.dirname(__file__)
+st_abs_file_path = os.path.join(script_dir, "static/")
+app.mount("/static", StaticFiles(directory=st_abs_file_path), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
 @app.get("/", response_class=HTMLResponse)
-async def index():
-    return JSONResponse(content={'massage': 'Hello geology!'}, status_code=200)
+async def index(request: Request):
+    try:
+        authorization: str = request.cookies.get("Authorization")
+        scheme, token = get_authorization_scheme_param(authorization)
+        if token:
+            return templates.TemplateResponse("personal.html", context={"request": request})
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='Could not validate credentials',
+                headers={'Authenticate': 'Bearer'})
+    except HTTPException:
+        return templates.TemplateResponse("index.html", context={"request": request})
 
 @app.on_event("startup")
 async def startup_event():
---
+++ src/app/static/js/requests.js
@@ -0,0 +1,87 @@
+function checkForm(username, password) {
+	if (username === '' || password === '') {
+		return false
+	} else {
+		return true
+	}
+}
+
+function login(username, password, gotoUrl = null) {
+	fetch('./auth/sign-in/', {
+		method: 'POST', // *GET, POST, PUT, DELETE, etc.
+		credentials: 'include', // include, *same-origin, omit
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/x-www-form-urlencoded',
+			// 'Content-Type': 'application/x-www-form-urlencoded',
+			'X-Requested-With': 'XMLHttpRequest',
+		},
+		body: `grant_type=password&username=${username}&password=${password}`,
+	}).then((response) => {
+		if (!response.ok) {
+			const inputs = document.querySelectorAll('#login-form input')
+			inputs.forEach((input) => {
+				input.classList.remove('is-valid')
+				input.classList.add('is-invalid')
+			})
+		} else {
+			if (gotoUrl) window.location.href = gotoUrl
+			if (!gotoUrl) window.location.reload()
+		}
+	})
+}
+
+const form = document.getElementById('login-form')
+
+if (form) {
+	form.addEventListener('submit', function (event) {
+		event.preventDefault()
+		event.stopPropagation()
+
+		const inputs = document.querySelectorAll('#login-form input')
+		inputs.forEach((input) => {
+			input.classList.remove('is-valid')
+			input.classList.remove('is-invalid')
+		})
+
+		let error = false
+
+		if (!checkForm(form.username.value, form.password.value)) {
+			inputs.forEach((input) => {
+				input.classList.add('is-invalid')
+			})
+			error = true
+		}
+
+		if (!error) login(form.username.value, form.password.value)
+	})
+}
+
+const btnOut = document.getElementById('log-out')
+if (btnOut) {
+	btnOut.addEventListener('click', (event) => {
+		event.preventDefault()
+		fetch('./auth/sign-out/', {
+			method: 'GET', // *GET, POST, PUT, DELETE, etc.
+			credentials: 'include', // include, *same-origin, omit
+		}).then(() => {
+			window.location.reload()
+		})
+	})
+
+	fetch('./auth/user/', {
+		method: 'GET', // *GET, POST, PUT, DELETE, etc.
+		credentials: 'include', // include, *same-origin, omit
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'X-Requested-With': 'XMLHttpRequest',
+		}
+	}).then((response) => {
+		if (!response.ok) {
+			btnOut.style.display = 'none'
+		} else {
+			btnOut.style.display = 'inline-flex'
+		}
+	})
+}
