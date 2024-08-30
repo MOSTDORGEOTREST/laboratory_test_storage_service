451ae8ccd2a616fa82e99eaedaf58efcf70896a0
Nick <tnick1502@mail.ru>
2024-08-30 16:31:45 +0300

    fix: auth

--- src/app/models/user.py
+++ src/app/models/user.py
@@ -4,8 +4,6 @@ class BaseUser(BaseModel):
     username: str = Field(..., description="The username of the user")
 
     class Config:
-        populate_by_name = True  # Allow using aliases for field names
-        str_strip_whitespace = True  # Automatically strip leading/trailing whitespace from strings
         from_attributes = True  # Enable ORM mode for compatibility with database models
 
 class User(BaseUser):
@@ -17,6 +15,4 @@ class Token(BaseModel):
     token_type: str = Field(default='bearer', description="Type of the token, default is 'bearer'")
 
     class Config:
-        populate_by_name = True  # Allow using aliases for field names
-        str_strip_whitespace = True  # Automatically strip leading/trailing whitespace from strings
         from_attributes = True  # Enable ORM mode for compatibility with database models
