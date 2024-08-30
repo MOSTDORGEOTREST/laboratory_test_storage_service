9742d2c13f1e320ee639fa72602b6d0daf32b02d
Nick <tnick1502@mail.ru>
2024-08-30 16:35:10 +0300

    Update user.py

--- src/app/models/user.py
+++ src/app/models/user.py
@@ -3,16 +3,9 @@ from pydantic import BaseModel, Field
 class BaseUser(BaseModel):
     username: str = Field(..., description="The username of the user")
 
-    class Config:
-        from_attributes = True  # Enable ORM mode for compatibility with database models
-
 class User(BaseUser):
-    # Inherits from BaseUser without additional fields or methods
     pass
 
 class Token(BaseModel):
     access_token: str = Field(..., description="The access token for authentication")
     token_type: str = Field(default='bearer', description="Type of the token, default is 'bearer'")
-
-    class Config:
-        from_attributes = True  # Enable ORM mode for compatibility with database models
